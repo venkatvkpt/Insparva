080a253f5326604d2a121d757552b6e9307aea71
unknown <venkatvkpt@gmail.com>
2017-12-13 08:55:22 +0530

    second commit

--- assets/js/validations.js
+++ assets/js/validations.js
@@ -1,9 +1,33 @@
+/* Ajax calls for existing checks*/
+
+
+
 /* confirm password checks*/
 
-$( "#checkConfirmPasswordStudent" ).change(function() {
-  alert( this.value );
-});
+var passwordCorporate = document.getElementById("passwordCorporate")
+, confirm_passwordCorporate = document.getElementById("checkConfirmPasswordCorporate");
 
-$( "#checkConfirmPasswordCorporate" ).change(function() {
-  alert( this.value );
-});
+function validatePasswordCorporate(){
+if(passwordCorporate.value != confirm_passwordCorporate.value) {
+  confirm_passwordCorporate.setCustomValidity("Passwords Don't Match");
+} else {
+  confirm_passwordCorporate.setCustomValidity('');
+}
+}
+
+passwordCorporate.onchange = validatePasswordCorporate;
+confirm_passwordCorporate.onkeyup = validatePasswordCorporate;
+
+var password = document.getElementById("passwordStudent")
+, confirm_password = document.getElementById("checkConfirmPasswordStudent");
+
+function validatePassword(){
+if(password.value != confirm_password.value) {
+  confirm_password.setCustomValidity("Passwords Don't Match");
+} else {
+  confirm_password.setCustomValidity('');
+}
+}
+
+password.onchange = validatePassword;
+confirm_password.onkeyup = validatePassword;
